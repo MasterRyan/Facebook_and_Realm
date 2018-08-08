5e0668c83b150c8cce02e33ad64eb787fd2fd888
user141656 <user141656@eu249.local>
2018-08-08 19:46:38 +0100

    Moving setup to facebook manager

--- FBLogin Realm/facebook/FacebookManager.swift
+++ FBLogin Realm/facebook/FacebookManager.swift
@@ -16,6 +16,15 @@ struct Page {
 
 class FacebookManager: NSObject {
 
+    public static func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) {
+        // Override point for customization after application launch.
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    public static func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
+    }
+
     static public func loginButton() -> FBSDKLoginButton {
 
         let loginButton = FBSDKLoginButton()
