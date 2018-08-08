789ad34dcbba36bbc06641c26dedcce5dde1048a
user141656 <user141656@eu249.local>
2018-08-08 21:51:23 +0100

    finalising loading control

--- FBLogin Realm/View Controllers/LoadingViewController.swift
+++ FBLogin Realm/View Controllers/LoadingViewController.swift
@@ -12,16 +12,13 @@ class LoadingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let logoutButton = FacebookManager.loginButton()
-        logoutButton.center = self.view.center
-        self.view.addSubview(logoutButton)
         // Do any additional setup after loading the view.
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if FacebookManager.isLoggedin() {
-            //go to app
+            performSegue(withIdentifier: "enterApp", sender: nil)
             return
         }
         performSegue(withIdentifier: "showLogin", sender: nil)
@@ -32,16 +29,5 @@ class LoadingViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
